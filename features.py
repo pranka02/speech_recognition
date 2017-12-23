b98a37dc2908200a4f9f735cfe79108a7e3c346a
Priyanka Mohandas <priyankamohandas02@gmail.com>
2017-12-22 19:47:28 -0500

    Add files via upload

---
+++ features.py
@@ -0,0 +1,35 @@
+import librosa
+import librosa.display
+from librosa import feature as ft
+import matplotlib.pyplot as plt
+import os 
+import numpy as np
+import csv
+import ntpath
+
+# root path
+root = os.path.dirname(os.path.realpath(__file__))
+path_name = r'train\audio'
+direc_name = os.path.join(root,path_name)
+train_path = r'train'
+csv_file = os.path.join(train_path,'features.csv')
+folders = os.listdir(path_name)
+print(folders)
+
+
+with open(csv_file, "w", newline= '') as output:
+	for folder in folders:
+		audio_class_folder = os.path.join(direc_name,folder)
+		files = os.listdir(audio_class_folder)
+		print(folder)
+		for file in files:
+			X, samp_rate = librosa.load(os.path.join(audio_class_folder,file))
+			stft = np.array(np.abs(librosa.stft(X)))
+			mfcc = np.array(np.mean(librosa.feature.mfcc(y=X, sr=samp_rate, n_mfcc=40).T,axis=0))
+			chroma = np.array(np.mean(librosa.feature.chroma_stft(S=stft, sr=samp_rate).T,axis=0))
+			contrast = np.array(np.mean(librosa.feature.spectral_contrast(S=stft, sr=samp_rate).T,axis=0))
+			features = (np.hstack((mfcc,chroma,contrast))).tolist()
+			features.append(folder)
+			writer = csv.writer(output, delimiter=',')
+			writer.writerow(features)
+print('Yay!')
