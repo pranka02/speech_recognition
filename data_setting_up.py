e581983eebea0e8f90fe4e641eebaa0d3b9ad3b6
Priyanka Mohandas <priyankamohandas02@gmail.com>
2018-07-19 14:13:06 -0400

    Add files via upload

--- data_setting_up.py
+++ data_setting_up.py
@@ -2,19 +2,19 @@ import numpy as np
 import os
 
 root = os.path.dirname(os.path.realpath(__file__))
-path_name = r'train\audio\unknown'
+path_name = r'train_full\audio\unknown_d'
 direc_name= os.path.join(root, path_name)
 files = os.listdir(direc_name)
 print(len(files))
 # file = os.listdir(os.path.join(dir_name,files[1]))
 # print(len(file))
-unknown_d ='unknown_d'
-new_fldr = os.path.join(direc_name, unknown_d)
+unknown ='unknown'
+new_fldr = os.path.join(direc_name, unknown)
 if not os.path.exists(new_fldr):
     os.makedirs(new_fldr)
 print(new_fldr)
 os.chdir(new_fldr)
 for file in files:
 	file_path = os.path.join(direc_name, file)
-	[os.rename(os.path.join(file_path, f), file+'-'+f) for f in os.listdir(file_path)]
+	[os.rename(os.path.join(file_path, f), file+'/'+f) for f in os.listdir(file_path)]
 
